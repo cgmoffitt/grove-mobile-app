ee42c1d821cf2206d57f9c4ebb584a1941bbf45c
cgmoffitt <chris.moffitt16@gmail.com>
2022-02-20 08:27:30 -0800

    move ActionButton to shared-components and use commonStyles in Home.js

--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { View, Pressable, Text, StyleSheet, Image, Dimensions } from "react-native";
-import { DARK_GREEN, CREME_WHITE, shadows } from "../constants/themes";
-import  ActionButton from "./utils/ActionButton";
+import { View, StyleSheet, Image, Dimensions } from "react-native";
+import ActionButton from "../components/shared-components/ActionButton";
+import commonStyles from "../styles/commonStyles"
+
 const Home = ({
-    navigation
+  navigation
 }) => {
   return (
-    <View style={styles.center}>
+    <View style={commonStyles.center}>
       <Image style={styles.plantLogo} source={require("../assets/images/plants/plant2.png")}></Image>
       <View style={styles.floatBottom}>
-      <ActionButton main="🌿  Plant an Activity  ☀️" onPressMethod={() => navigation.navigate("PlantActivity")}
-      ></ActionButton>
-
+        <ActionButton main="🌿  Plant an Activity  ☀️" onPressMethod={() => navigation.navigate("PlantActivity")}
+        ></ActionButton>
       </View>
     </View>
   );
@@ -21,22 +21,14 @@ const windowWidth = Dimensions.get('window').width;
 const logoWidth = windowWidth * 0.8;
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    backgroundColor: CREME_WHITE,
-    position: "relative"
-  },
   plantLogo: {
-    width:logoWidth,
-    height:logoWidth,
-    marginBottom:100
+    width: logoWidth,
+    height: logoWidth,
+    marginBottom: 100
   },
   floatBottom: {
     width: '100%',
-    justifyContent:"flex-end",
+    justifyContent: "flex-end",
     alignItems: "center",
     position: "absolute",
     bottom: 0,
